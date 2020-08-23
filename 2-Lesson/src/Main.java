e4566f72e14513254f9888b969d9c6c1f2c317ef
SergeyFedorov <Avel8985@gmail.com>
2020-08-23 13:03:53 +0300

    Lesson1 - JavaCoreOnelvl

--- 2-Lesson/src/Main.java
+++ 2-Lesson/src/Main.java
@@ -1,3 +1,23 @@
 public class Main {
 
+  public static void main(String[] args) {
+    // 1.
+//    int[] arr = { 1, 0, 1, 0, 0, 1 };
+//    invertArray(arr);
+//    for (int i : arr) {
+//      System.out.println(i);
+//    }
+
+  }
+  //1.
+  public static void invertArray(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == 1) {
+        arr[i]--;
+      } else {
+        arr[i]++;
+      }
+    }
+  }
+
 }
