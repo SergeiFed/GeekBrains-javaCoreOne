76cac3a73454bd2075e263b6688384d51d4589f0
SergeyFedorov <Avel8985@gmail.com>
2020-08-28 12:01:40 +0300

    TwoGame Three Lesson3

--- javaCoreOneLvl/Lesson3-Game/src/TwoGame.java
+++ javaCoreOneLvl/Lesson3-Game/src/TwoGame.java
@@ -1,2 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class TwoGame {
+    static String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+            "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear",
+            "pepper", "pineapple", "pumpkin", "potato"};
+    static Random random = new Random();
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        StringBuilder randomBuilder = new StringBuilder(words[random.nextInt(words.length)]);
+        System.out.println(randomBuilder);
+        while (true) {
+            StringBuilder playerText = new StringBuilder(scanner.nextLine());
+            StringBuilder slash = new StringBuilder("###############");
+
+            if (playerText.equals(randomBuilder)) {
+                System.out.println("Поздравляю вы угадали");
+                break;
+            } else {
+                for (int i = 0; i < playerText.length(); i++) {
+                    if (playerText.charAt(i) == randomBuilder.charAt(i)) {
+                        slash.setCharAt(i, playerText.charAt(i));
+                    }
+                }
+                System.out.println("Попробуйте еще раз вы угадали вот эти символы");
+                System.out.println(slash);
+            }
+        }
+    }
 }
+
